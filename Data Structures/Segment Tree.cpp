d411fe759dd5f7e6cc322d9ddb3dab3ac5d6b219
Sola Stunfell <trxbach135@gmail.com>
2019-06-15 18:37:35 +0700

    Create Segment Tree.cpp

---
+++ Data Structures/Segment Tree.cpp
@@ -0,0 +1,92 @@
+int a[N];
+
+struct segment_tree{ // Sum query
+    int seg[4 * N];
+    
+    void build(int id, int l, int r){
+        if (l == r){
+            seg[id] = a[l];
+            return;
+        }
+        int mid = (l + r) >> 1;
+        build(id << 1, l, mid);
+        build(id << 1 | 1, mid + 1, r);
+        seg[id] = seg[id << 1] + seg[id << 1 | 1];
+    }
+    
+    void upd(int id, int l, int r, int i, int val){
+        if (i < l || r < i){
+            return;
+        }
+        if (l == r){
+            seg[id] += val;
+            return;
+        }
+        int mid = (l + r) >> 1;
+        upd(id << 1, l, mid, i, val);
+        upd(id << 1 | 1, mid + 1, r, i, val);
+        seg[id] = seg[id << 1] + seg[id << 1 | 1];
+    }
+    
+    int get(int id, int l, int r, int u, int v){
+        if (v < l || r < u){
+            return 0;
+        }
+        if (u <= l && r <= v){
+            return seg[id];
+        }
+        int mid = (l + r) >> 1;
+        return get(id << 1, l, mid, u, v) + get(id << 1 | 1, mid + 1, r, u, v);
+    }
+} it1;
+
+struct lazy_segment_tree{ // Sum query
+    int seg[4 * N], lazy[4 * N];
+    
+    void down(int id, int l, int r){
+        int mid = (l + r) >> 1;
+        lazy[id << 1] += lazy[id];
+        lazy[id << 1 | 1] += lazy[id];
+        seg[id << 1] += lazy[id] * (mid - l + 1);
+        seg[id << 1 | 1] += lazy[id] * (r - mid);
+        lazy[id] = 0;
+    }
+    
+    void build(int id, int l, int r){
+        if (l == r){
+            seg[id] = a[l];
+            return;
+        }
+        int mid = (l + r) >> 1;
+        build(id << 1, l, mid);
+        build(id << 1 | 1, mid + 1, r);
+        seg[id] = seg[id << 1] + seg[id << 1 | 1];
+    }
+    
+    void upd(int id, int l, int r, int i, int val){
+        if (i < l || r < i){
+            return;
+        }
+        if (l == r){
+            seg[id] += val;
+            return;
+        }
+        down(id, l, r);
+        int mid = (l + r) >> 1;
+        upd(id << 1, l, mid, i, val);
+        upd(id << 1 | 1, mid + 1, r, i, val);
+        seg[id] = seg[id << 1] + seg[id << 1 | 1];
+    }
+    
+    int get(int id, int l, int r, int u, int v){
+        if (v < l || r < u){
+            return 0;
+        }
+        if (u <= l && r <= v){
+            return seg[id];
+        }
+        down(id, l, r);
+        int mid = (l + r) >> 1;
+        return get(id << 1, l, mid, u, v) + get(id << 1 | 1, mid + 1, r, u, v);
+    }
+} it2;
