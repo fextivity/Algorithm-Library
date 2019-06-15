c290aa1abd5dc50e59266251b24c19ca2a8da1e6
Sola Stunfell <trxbach135@gmail.com>
2019-06-15 21:43:47 +0700

    Minor update

--- Graph/Shortest Path/Dijkstra.cpp
+++ Graph/Shortest Path/Dijkstra.cpp
@@ -8,14 +8,14 @@ void dijkstra(int u){
     pque <pii, vpii, greater <pii > > pq;
     mmset(h);
     pq.push({0, u});
-    h[u] = 0;
+    dist[u] = 0;
     while (!pq.empty()){
         int t = pq.top().se;
         pq.pop();
         Fora(v, adj[t]){
-            if (h[v.fi] > h[t] + v.se){
-                h[v.fi] = h[t] + v.se;
-                pq.push({h[v.fi], v.fi});
+            if (dist[v.fi] > dist[t] + v.se){
+                dist[v.fi] = dist[t] + v.se;
+                pq.push({dist[v.fi], v.fi});
             }
         }
     }
