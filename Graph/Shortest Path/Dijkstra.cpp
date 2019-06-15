f7fcf573fca0065ca99bcc897ac52551b9ae8da8
Sola Stunfell <trxbach135@gmail.com>
2019-06-15 21:24:43 +0700

    Create Dijkstra.cpp

---
+++ Graph/Shortest Path/Dijkstra.cpp
@@ -0,0 +1,20 @@
+int n;
+int h[N];
+vpii adj[N]; // fi : node label, se: edge weight
+
+void dijkstra(int u){
+    pque <pii, vpii, greater <pii > > pq;
+    mmset(h);
+    pq.push({0, u});
+    h[u] = 0;
+    while (!pq.empty()){
+        int t = pq.top().se;
+        pq.pop();
+        Fora(v, adj[t]){
+            if (h[v.fi] > h[t] + v.se){
+                h[v.fi] = h[t] + v.se;
+                pq.push({h[v.fi], v.fi});
+            }
+        }
+    }
+}
