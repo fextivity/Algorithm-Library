88b187a10c450b09aa59b2b1a58d2f092f332d62
Sola Stunfell <trxbach135@gmail.com>
2019-06-15 21:47:17 +0700

    Create Floyd-Warshall.cpp

---
+++ Graph/Shortest Path/Floyd-Warshall.cpp
@@ -0,0 +1,20 @@
+int n;
+int adj[N][N]; // 0 if i = j, inf if there is no edge, else weight of edge
+int dist[N][N];
+
+void floyd(){
+    ForE(i, 1, n){
+        ForE(j, 1, n){
+            dist[i][j] = adj[i][j];
+        }
+    }
+    ForE(k, 1, n){
+        ForE(i, 1, n){
+            ForE(j, 1, n){
+                if (dist[i][k] + dist[k][j] < dist[i][j]){
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+}
