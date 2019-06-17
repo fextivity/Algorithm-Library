3c6c265c0f0e7b02a135efc8dccd61c8926337d6
Sola Stunfell <trxbach135@gmail.com>
2019-06-17 15:28:51 +0700

    Create BFS.cpp

---
+++ Graphs/Graph traversal/BFS.cpp
@@ -0,0 +1,18 @@
+vi adj[N];
+bool ck[N];
+
+// Complexity: O(V)
+void bfs(int u){
+    queue <int> qu;
+    qu.push(u);
+    while (!qu.empty()){
+        int t = qu.front();
+        qu.pop();
+        ck[t] = 1;
+        Fora(v, adj[t]){
+            if (!ck[v]){
+                qu.push(v);
+            }
+        }
+    }
+}
