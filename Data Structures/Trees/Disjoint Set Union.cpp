a26124bb70b54b097b0db2f80b0de66ac8370520
Sola Stunfell <trxbach135@gmail.com>
2020-01-04 09:36:58 +0700

    Create Disjoint Set Union.cpp

---
+++ Data Structures/Trees/Disjoint Set Union.cpp
@@ -0,0 +1,16 @@
+int par[N];
+
+int root(int x){
+    return par[x] < 0 ? x : par[x] = root(par[x]);
+}
+
+void merge(int x, int y){
+    if ((x = root(x)) == (y = root(y))){
+        return;
+    }
+    if (par[y] < par[x]){
+        swap(x, y);
+    }
+    par[x] += par[y];
+    par[y] = x;
+}
