e0cb4e11a80d2e7d33fc921ac46a09c526542b9e
Sola Stunfell <trxbach135@gmail.com>
2020-01-04 10:01:54 +0700

    Create Fenwick Tree.cpp

---
+++ Data Structures/Trees/Fenwick Tree.cpp
@@ -0,0 +1,35 @@
+struct FenwickTree{
+    vi bit;
+    int n;
+
+    FenwickTree(int n = 0){
+        this->n = n + 1;
+        bit.assign(n + 1, 0);
+    }
+
+    FenwickTree(vector<int> a) : FenwickTree(a.size()){
+        this->n = a.size() + 1;
+        bit.assign(n + 1, 0);
+        For(i, 0, a.size()){
+            add(i, a[i]);
+        }
+    }
+
+    int sum(int idx){
+        int ans = 0;
+        for (++idx; idx > 0; idx -= idx & -idx){
+            ans += bit[idx];
+        }
+        return ans;
+    }
+
+    int sum(int l, int r){
+        return sum(r) - sum(l - 1);
+    }
+
+    void add(int idx, int delta){
+        for (++idx; idx < n; idx += idx & -idx){
+            bit[idx] += delta;
+        }
+    }
+} bit1;
