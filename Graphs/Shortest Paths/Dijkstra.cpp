263b9206da7d03e969b139e80c0e3d93ab7fc843
Sola Stunfell <trxbach135@gmail.com>
2020-01-12 11:08:49 +0700

    Update Dijkstra.cpp

--- Graphs/Shortest Paths/Dijkstra.cpp
+++ Graphs/Shortest Paths/Dijkstra.cpp
@@ -6,12 +6,13 @@ vpii adj[N]; // fi : node label, se: edge weight
 // Complexity: O(ElogE)
 void dijkstra(int u){
     pque <pii, vpii, greater <pii > > pq;
-    mmset(h);
+    mmset(dist);
     pq.push({0, u});
     dist[u] = 0;
     while (!pq.empty()){
-        int t = pq.top().se;
+        int t = pq.top().se, dt = pq.top().fi;
         pq.pop();
+        if (dt != dist[t]) continue;
         Fora(v, adj[t]){
             if (dist[v.fi] > dist[t] + v.se){
                 dist[v.fi] = dist[t] + v.se;
