770ac87e3b14d23644b9322198d9b7ec8a28a60a
Sola Stunfell <trxbach135@gmail.com>
2020-01-14 12:02:04 +0700

    Create Finding Articulation Points.cpp

---
+++ Graphs/Connected components, bridges, articulations points/Finding Articulation Points.cpp
@@ -0,0 +1,44 @@
+int n;
+vi adj[N];
+int tin[N], low[N];
+bool ck[N], iscutpoint[N];
+int tim;
+
+void dfs(int u, int p){
+    ck[u] = 1;
+    tin[u] = tim;
+    low[u] = tim;
+    tim++;
+    int child = 0;
+    Fora(v, adj[u]){
+        if (v == p){
+            continue;
+        }
+        if (ck[v]){
+            low[u] = min(low[u], tin[v]);
+        }
+        else{
+            dfs(v, u);
+            low[u] = min(low[u], low[v]);
+            if (low[v] >= tin[u] && p != -1){
+                iscutpoint[u] = 1;
+            }
+            child++;
+        }
+    }
+    if (p == -1 && child > 1){
+        iscutpoint[u] = 1;
+    }
+}
+
+void find_cutpoints(){
+    tim = 0;
+    mset(ck);
+    msetn1(tin);
+    msetn1(low);
+    For(i, 0, n){
+        if (!ck[i]){
+            dfs(i, -1);
+        }
+    }
+}
