0598e47b3f56137ac58b7f42792a590b12aee186
Sola Stunfell <trxbach135@gmail.com>
2020-01-21 11:59:05 +0700

    Create Z-function.cpp

---
+++ String Processing/Fundamentals/Z-function.cpp
@@ -0,0 +1,33 @@
+int n;
+string s;
+int z[N];
+
+void z_function(){
+    Z[1] = 0;
+    int l = 0, r = 0;
+    ForE(i, 2, n){
+        if (r < i){
+            l = i;
+            r = i;
+            while (r <= n && s[r] == s[r - l + 1]){
+                r++;
+            }
+            Z[i] = r - l;
+            r--;
+        }
+        else{
+            int k = i - l + 1;
+            if (Z[k] < r - i){
+                Z[i] = Z[k];
+            }
+            else{
+                l = i;
+                while (r <= n && s[r] == s[r - l + 1]){
+                    r++;
+                }
+                Z[i] = r - l;
+                r--;
+            }
+        }
+    }
+}
