ea68c5fb3797c1222b263eaf7180a4a5984e2e8f
Sola Stunfell <trxbach135@gmail.com>
2020-01-25 21:24:27 +0700

    Create Topological Sorting.cpp

---
+++ Graphs/Miscellaneous/Topological Sorting.cpp
@@ -0,0 +1,22 @@
+int n;
+vi adj[N];
+bool ck[N];
+vi ans;
+
+void dfs(int u){
+    Fora(v, adj[u]){
+        if (!ck[v]){
+            dfs(v);
+        }
+    }
+    ans.pb(u);
+}
+
+void toposort(){
+    ForE(i, 1, n){
+        if (!ck[i]){
+            dfs(i);
+        }
+    }
+    reverse(bend(ans));
+}
