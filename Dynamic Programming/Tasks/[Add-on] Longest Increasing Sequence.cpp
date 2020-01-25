7346d78d1e19f15f902dd2081b4cf903202239d1
Sola Stunfell <trxbach135@gmail.com>
2020-01-25 12:18:45 +0700

    Create [Add-on] Longest Increasing Sequence.cpp

---
+++ Dynamic Programming/Tasks/[Add-on] Longest Increasing Sequence.cpp
@@ -0,0 +1,18 @@
+int n;
+int a[N];
+
+int lis(){
+    int b[N];
+    int idx = 1;
+    b[idx] = a[1];
+    ForE(i, 2, n){
+        if (a[i] >= b[idx]){
+            idx++;
+            b[idx] = a[i];
+            continue;
+        }
+        int itr = lwb(b + 1, b + 1 + idx, a[i]) - b;
+        b[itr] = a[i];
+    }
+    return idx;
+}
