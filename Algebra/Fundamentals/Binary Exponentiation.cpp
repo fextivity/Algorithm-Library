5aa9767356de137046edb292390ae8ac6b46861f
Sola Stunfell <trxbach135@gmail.com>
2020-02-15 19:12:02 +0700

    Update Binary Exponentiation.cpp

--- Algebra/Fundamentals/Binary Exponentiation.cpp
+++ Algebra/Fundamentals/Binary Exponentiation.cpp
@@ -1,4 +1,14 @@
-// Complexity: O(log(y))
+/*
+1. File: Binary Exponentiation.cpp
+2. Algorithm
+Calculate power (x^y) with arbitrary base (x), exponent (y) and possibly modulo (mod).
+Use Divide and Conquer (D&C).
+3. Usage and Applications
+Function binpow(int x, int y) return value of (x^y) modulo mod.
+Useful for general algebra with numbers upto 10^9.
+4. Complexity: O(log(y)).
+*/
+
 int binpow(int x, int y){
     int ans = 1;
     while (y){
