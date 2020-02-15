b560f33c3979dba7d615511634c951434a86094d
Sola Stunfell <trxbach135@gmail.com>
2020-02-15 19:22:19 +0700

    Update Extended Euclidean Algorithm.cpp

--- Algebra/Fundamentals/Extended Euclidean Algorithm.cpp
+++ Algebra/Fundamentals/Extended Euclidean Algorithm.cpp
@@ -1,3 +1,14 @@
+/*
+1. File: Extended Euclidean Algorithm.cpp   Link: https://cp-algorithms.com/algebra/extended-euclid-algorithm.html
+2. Algorithm
+Calculate gcd of two integers (a and b), but also calculate coefficients x and y such that ax + by = 1.
+3. Usage and Applications
+Function gcd(int a, int b, int &x, int &y) return gcd of a and b, but also modify value of x and y.
+Useful for general algebra.
+4. Complexity: O(log(min(a,b)).
+*/
+
+
 int gcd(int a, int b, int &x, int &y){
     if (a == 0){
         x = 0;
