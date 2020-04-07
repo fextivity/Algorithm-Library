c09165b6d40b30403cef5d464c4afc6b605ab856
Sola Stunfell <trxbach135@gmail.com>
2020-04-07 16:47:23 +0700

    Update Sieve of Eratosthenes with Linear Complexity.cpp

--- Algebra/Prime numbers/Sieve of Eratosthenes with Linear Complexity.cpp
+++ Algebra/Prime numbers/Sieve of Eratosthenes with Linear Complexity.cpp
@@ -3,12 +3,12 @@ int lpf[N];
 vector <int> prime;
 
 void sieve(){
-    for (int i = 2; i <= n; i++){
+    for (int i = 2; i < N; i++){
         if (lpf[i] == 0){
             lpf[i] = i;
             prime.pb(i);
         }
-        for (int j = 0; j < prime.size() && prime[j] <= lpf[i] && i * prime[j] <= n; j++){
+        for (int j = 0; j < prime.size() && prime[j] <= lpf[i] && i * prime[j] < N; j++){
             lpf[i * prime[j]] = prime[j];
         }
     }
