e362e0c144ec8052ea0e927e505fd860685b02f8
Sola Stunfell <trxbach135@gmail.com>
2020-06-11 09:39:57 +0700

    Update Disjoint Set Union.cpp

--- Data Structures/Trees/Disjoint Set Union.cpp
+++ Data Structures/Trees/Disjoint Set Union.cpp
@@ -20,25 +20,3 @@ struct disjoint_set_union{
         par[y] = x;
     }
 } dsu;
-
-
-int par[N];
-
-void init(){
-    msetn1(par);
-}
-
-int root(int x){
-    return par[x] < 0 ? x : par[x] = root(par[x]);
-}
-
-void merge(int x, int y){
-    if ((x = root(x)) == (y = root(y))){
-        return;
-    }
-    if (par[y] < par[x]){
-        swap(x, y);
-    }
-    par[x] += par[y];
-    par[y] = x;
-}
