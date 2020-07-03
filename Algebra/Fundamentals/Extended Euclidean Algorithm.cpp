e4463e2a747091a4deab1b9800b058246b6c80ff
Sola Stunfell <trxbach135@gmail.com>
2020-07-03 18:43:19 +0700

    Update Extended Euclidean Algorithm.cpp

--- Algebra/Fundamentals/Extended Euclidean Algorithm.cpp
+++ Algebra/Fundamentals/Extended Euclidean Algorithm.cpp
@@ -5,17 +5,16 @@ Calculate gcd of two integers (a and b), but also calculate coefficients x and y
 3. Usage and Applications
 Function gcd(int a, int b, int &x, int &y) return gcd of a and b, but also modify value of x and y.
 Useful for general algebra.
-4. Complexity: O(log(min(a,b)).
+4. Complexity: O(log(min(a, b)).
 */
 
-
-int gcd(int a, int b, int &x, int &y){
+long long gcd(long long a, long long b, long long &x, long long &y){
     if (a == 0){
         x = 0;
         y = 1;
         return b;
     }
-    int x1, y1, d = gcd(b % a, a, x1, y1);
+    long long x1, y1, d = __gcd(b % a, a, x1, y1);
     x = y1 - (b / a) * x1;
     y = x1;
     return d;
