3cc06c0138ac28adc3779a3baa9353dc9527ca7c
Sola Stunfell <trxbach135@gmail.com>
2020-07-03 18:44:52 +0700

    Update Fibonacci Numbers.cpp

--- Algebra/Fundamentals/Fibonacci Numbers.cpp
+++ Algebra/Fundamentals/Fibonacci Numbers.cpp
@@ -1,8 +1,8 @@
-pii fib(int n){
-    if (n == 0) return {0, 1};
-    pii p = fib(n >> 1);
-    int c = p.fi * (2 * p.se - p.fi);
-    int d = p.fi * p.fi + p.se * p.se;
-    if (n & 1) return {d, c + d};
-    return {c, d};
+pair <long long, long long> fib(long long n){
+    if (n == 0) return make_pair(0, 1);
+    pair <long long, long long> p = fib(n >> 1);
+    long long c = p.first * (2 * p.second - p.fifst);
+    long long d = p.first * p.first + p.second * p.second;
+    if (n & 1) return make_pair(d, c + d);
+    return make_pair(c, d);
 }
