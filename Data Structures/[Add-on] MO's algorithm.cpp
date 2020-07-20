097d55e19c165a30e44307f37a34754f24b64d45
Sola Stunfell <trxbach135@gmail.com>
2020-07-20 09:40:02 +0700

    Update [Add-on] MO's algorithm.cpp

--- Data Structures/[Add-on] MO's algorithm.cpp
+++ Data Structures/[Add-on] MO's algorithm.cpp
@@ -1,33 +1,33 @@
 ll gilbertOrder(int x, int y, int pow, int rotate) {
-	if (pow == 0) {
-		return 0;
-	}
-	int hpow = 1 << (pow-1);
-	int seg = (x < hpow) ? (
-		(y < hpow) ? 0 : 3
-	) : (
-		(y < hpow) ? 1 : 2
-	);
-	seg = (seg + rotate) & 3;
-	const int rotateDelta[4] = {3, 0, 0, 1};
-	int nx = x & (x ^ hpow), ny = y & (y ^ hpow);
-	int nrot = (rotate + rotateDelta[seg]) & 3;
-	ll subSquareSize = int64_t(1) << (2*pow - 2);
-	ll ans = seg * subSquareSize;
-	ll add = gilbertOrder(nx, ny, pow-1, nrot);
+    if (pow == 0) {
+        return 0;
+    }
+    int hpow = 1 << (pow-1);
+    int seg = (x < hpow) ? (
+        (y < hpow) ? 0 : 3
+    ) : (
+        (y < hpow) ? 1 : 2
+    );
+    seg = (seg + rotate) & 3;
+    const int rotateDelta[4] = {3, 0, 0, 1};
+    int nx = x & (x ^ hpow), ny = y & (y ^ hpow);
+    int nrot = (rotate + rotateDelta[seg]) & 3;
+    ll subSquareSize = int64_t(1) << (2*pow - 2);
+    ll ans = seg * subSquareSize;
+    ll add = gilbertOrder(nx, ny, pow-1, nrot);
     ans += (seg == 1 || seg == 2) ? add : (subSquareSize - add - 1);
-	return ans;
+    return ans;
 }
 
 struct Query {
-	int l, r, idx;
-	ll ord;
+    int l, r, idx;
+    ll ord;
 
-	void calcOrder() {
-		ord = gilbertOrder(l, r, 21, 0);
-	}
+    void calcOrder() {
+        ord = gilbertOrder(l, r, 21, 0);
+    }
 };
 
 bool operator<(const Query &a, const Query &b) {
-	return a.ord < b.ord;
+    return a.ord < b.ord;
 }
