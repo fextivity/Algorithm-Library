1e00297ca8c868f79d6129421679568db5933ef8
Sola Stunfell <trxbach135@gmail.com>
2020-08-15 16:40:22 +0700

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -1,3 +1,26 @@
+#include <string>
+#include <bits/functexcept.h>
+#include <iosfwd>
+#include <bits/cxxabi_forced.h>
+#include <bits/functional_hash.h>
+
+#pragma push_macro("__SIZEOF_LONG__")
+#pragma push_macro("__cplusplus")
+#define __SIZEOF_LONG__ __SIZEOF_LONG_LONG__
+#define unsigned unsigned long
+#define __cplusplus 201102L
+
+#define __builtin_popcountl __builtin_popcountll
+#define __builtin_ctzl __builtin_ctzll
+
+#include <bitset>
+
+#pragma pop_macro("__cplusplus")
+#pragma pop_macro("__SIZEOF_LONG__")
+#undef unsigned
+#undef __builtin_popcountl
+#undef __builtin_ctzl
+
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
